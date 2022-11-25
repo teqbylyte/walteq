c3415c811865beefb75619abe31f819a1a19fb47
Lyte Onyema <lyte.onyema@gmail.com>
2022-11-26 00:24:03 +0100

    Set up wallet

---
+++ app/Models/Wallet.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Wallet extends Model
+{
+    use HasFactory, HasUuids, SoftDeletes;
+
+    protected $guarded = ['id', 'account_number'];
+
+    public function getRouteKeyName(): string
+    {
+        return 'account_number';
+    }
+}
---
+++ database/migrations/2022_11_25_230407_create_wallets_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wallets', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->string('email')->unique();
+            $table->string('account_number')->unique();
+            $table->double('balance')->default(0);
+            $table->enum('status', ['ACTIVE', 'SUSPENDED', 'INACTIVE'])->default('ACTIVE');
+            $table->boolean('disable_debit')->default(false);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wallets');
+    }
+};
