da5e56cd56b3e0c1351d6390208bd7ff5171b8c2
Lyte Onyema <lyte.onyema@gmail.com>
2022-11-30 09:54:10 +0100

    Add date input for filtering

--- resources/views/pages/wallets/transactions.blade.php
+++ resources/views/pages/wallets/transactions.blade.php
@@ -2,6 +2,16 @@
 
 @section('title', 'Wallets')
 
+@section('vendor-style')
+    <link rel="stylesheet" href="{{ asset(mix('vendors/css/pickers/pickadate/pickadate.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('vendors/css/pickers/flatpickr/flatpickr.min.css')) }}">
+@endsection
+
+@push('page-style')
+    <link rel="stylesheet" href="{{ asset(mix('css/base/plugins/forms/pickers/form-flat-pickr.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('css/base/plugins/forms/pickers/form-pickadate.css')) }}">
+@endpush
+
 @section('content')
     <div class="row">
         <div class="col-12">
@@ -11,3 +21,15 @@
 
     <livewire:wallet-transactions-table />
 @endsection
+
+
+@section('vendor-script')
+    <script src="{{ asset(mix('vendors/js/pickers/pickadate/picker.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/js/pickers/pickadate/picker.date.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/js/pickers/pickadate/legacy.js')) }}"></script>
+    <script src="{{ asset(mix('vendors/js/pickers/flatpickr/flatpickr.min.js')) }}"></script>
+@endsection
+
+@push('page-script')
+    <script src="{{ asset(mix('js/scripts/forms/pickers/form-pickers.js')) }}"></script>
+@endpush
