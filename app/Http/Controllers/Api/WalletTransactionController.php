46dbeb1fedb9fdb957305c3b4b4ece60ab4f89de
Lyte Onyema <lyte.onyema@gmail.com>
2022-11-30 08:56:47 +0100

    Add pagination for api request

--- app/Http/Controllers/Api/WalletTransactionController.php
+++ app/Http/Controllers/Api/WalletTransactionController.php
@@ -10,6 +10,6 @@ class WalletTransactionController extends Controller
 {
     public function index(Wallet $wallet)
     {
-        return MyResponse::success('Wallet transactions fetched!', $wallet->transactions);
+        return MyResponse::success('Wallet transactions fetched!', $wallet->transactions()->simplePaginate(75));
     }
 }
